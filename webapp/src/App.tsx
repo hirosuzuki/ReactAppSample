387b50c425a64523cab612d3a893dff525835408
hirosuzuki <hiro.suzuki.oct10@gmail.com>
2023-06-18 08:43:56 +0000

    MUI 追加

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,8 +1,11 @@
+import Button from "@mui/material/Button";
+
 const App = () => {
   return (
     <>
       <div>
         <h1>React App Sample</h1>
+        <Button variant="contained">Hello World</Button>
       </div>
     </>
   );
